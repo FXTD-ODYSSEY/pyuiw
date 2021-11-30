d6aacf61f7fbf9af91fa77f9861bf9c6a5ebca97
timmyliang <timmyliang@tencent.com>
2021-11-30 22:42:10 +0800

    build: cleanup tox env

---
+++ scripts/install.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+# Import future modules
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Import built-in modules
+import subprocess
+import sys
+
+# Import third-party modules
+import toml
+
+
+__author__ = "timmyliang"
+__email__ = "820472580@qq.com"
+__date__ = "2021-11-30 22:33:11"
+
+
+with open("pyproject.toml") as f:
+    data = toml.load(f)
+
+version = data["tool"]["poetry"]["version"]
+
+subprocess.call(["poetry", "build"])
+subprocess.call(["python", "-m", "pip", "uninstall", "-y", "pyuiw"])
+subprocess.call(
+    ["python", "-m", "pip", "install", f"dist/pyuiw-{version}-py3-none-any.whl"]
+)
+subprocess.call(["pyuiw", "tests/ui/custom.ui"])
--- scripts/run.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-setlocal
-set PYTHONPATH=%cd%
-python pyuiw/__main__.py -ni -nb f:/light_git/MAvatar/pose_editor/ui/window.ui
-endlocal
